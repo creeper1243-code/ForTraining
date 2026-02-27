eee213994e92e304180217dbc27d828f983d3680
Cristian <cristian@bratash.net>
2026-02-27 18:20:34 +0100

    "for" for list, word, custom range

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 for i in range(5):
     print(f"{i} HI CACITA")
+
+list = ["supercalifragilisticexpialadocious", "chargoggagoggmanchauggagoggchaubunagungamaugg", "pneumonoultramicroscopicsiliconovolcaniconiosis"]
+for word in list:
+    print(f"my word is {word}")
+
+word = "supercalifragilisticexpialadocious"
+for letter in word:
+    print(f"<{letter}>", end="")
+
+print()
+
+for i in range(1, 5):
+    print(f"{i} cacita feliz")
